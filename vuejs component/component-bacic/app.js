56530e08cedbf020ee2c3d9e03d9f237a7beaaa6
duongnq2798 <duongnq27.98@gmail.com>
2020-07-26 14:30:35 +0700

    Component

---
+++ vuejs component/component-bacic/app.js
@@ -0,0 +1,15 @@
+/**
+ * Vue.component('name_component' , {object})
+ */
+Vue.component('click-counter', {
+  template: '<button @click="count++"> {{count}} </button>',
+  data() {
+    return {
+      count: 0,
+    }
+  },
+})
+
+new Vue({
+  el: '#app',
+})
